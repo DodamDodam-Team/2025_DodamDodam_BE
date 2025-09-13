0c37dcde551f86002a8fbf7f97619616b4f382ff
nanakyeong <dlskrud468@naver.com>
2025-09-14 02:33:34 +0900

    feat: 깃허브 연동

---
+++ src/test/java/com/dodamdodam/dodamdodam/DodamdodamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodamdodam.dodamdodam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodamdodamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
