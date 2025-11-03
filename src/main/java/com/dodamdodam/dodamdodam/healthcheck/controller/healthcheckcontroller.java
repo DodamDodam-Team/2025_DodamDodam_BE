3bd1a7612e7372dbd4f5ad22b45fa7b1c84aaeb8
ondacloud <junseong8299@gmail.com>
2025-11-03 20:37:11 +0900

    feat: Add Health Check API

---
+++ src/main/java/com/dodamdodam/dodamdodam/healthcheck/controller/healthcheckcontroller.java
@@ -0,0 +1,16 @@
+package com.dodamdodam.dodamdodam.healthcheck.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+public class HealthCheckController {
+
+	@GetMapping("/healthcheck")
+	public ResponseEntity<Map<String, String>> healthcheck() {
+		return ResponseEntity.ok(Collections.singletonMap("status", "ok"));
+	}
+
+}
