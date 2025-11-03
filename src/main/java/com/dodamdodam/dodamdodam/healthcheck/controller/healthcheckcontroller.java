5122e71f26f67512904280d6521c42440dc9e5c0
ondacloud <junseong8299@gmail.com>
2025-11-03 20:42:58 +0900

    fix: Modify Health Check API

--- src/main/java/com/dodamdodam/dodamdodam/healthcheck/controller/healthcheckcontroller.java
+++ src/main/java/com/dodamdodam/dodamdodam/healthcheck/controller/healthcheckcontroller.java
@@ -2,15 +2,15 @@ package com.dodamdodam.dodamdodam.healthcheck.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.Collections;
 import java.util.Map;
 
 @RestController
 public class HealthCheckController {
 
-	@GetMapping("/healthcheck")
-	public ResponseEntity<Map<String, String>> healthcheck() {
-		return ResponseEntity.ok(Collections.singletonMap("status", "ok"));
-	}
-
+    @GetMapping("/healthcheck")
+    public ResponseEntity<Map<String, String>> healthcheck() {
+        return ResponseEntity.ok(Collections.singletonMap("status", "ok"));
+    }
 }
