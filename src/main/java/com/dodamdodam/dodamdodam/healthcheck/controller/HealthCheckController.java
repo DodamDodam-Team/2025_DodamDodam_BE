a36fb8e8a42b35df3125871f72d239b85b5445b7
ondacloud <junseong8299@gmail.com>
2025-11-04 16:06:39 +0900

    fix: Health Check API

--- src/main/java/com/dodamdodam/dodamdodam/healthcheck/controller/HealthCheckController.java
+++ src/main/java/com/dodamdodam/dodamdodam/healthcheck/controller/HealthCheckController.java
@@ -2,6 +2,7 @@ package com.dodamdodam.dodamdodam.healthcheck.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController; 
 import java.util.Collections;
 import java.util.Map;
 
